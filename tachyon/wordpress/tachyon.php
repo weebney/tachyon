4a4bd5c7e1777fe2006aee93da363cd848c9f255
weebney <w@freeassn.org>
2023-03-02 14:28:14 -0500

    Refactor forEach to map/filter & fix #24

--- tachyon/wordpress/tachyon.php
+++ tachyon/wordpress/tachyon.php
@@ -3,7 +3,7 @@
 * Plugin Name: Tachyon
 * Plugin URI: https://fasterthanlight.net/
 * Description: Make your WordPress site faster than light.
-* Version: 1.0.1
+* Version: 1.1.0
 * Author: weebney
 * Author URI: https://github.com/weebney
 * License: MIT
@@ -12,7 +12,7 @@
 /* Start Adding Functions Below this Line */
 function wpb_hook_javascript() {
     ?>
-    <script src="https://unpkg.com/tachyonjs@1.0.1/tachyon.min.js" integrity="sha384-YzRhaEvO+tu7clAuAStGkSR0bVr2+IIq1gXxi28k2Wj0gGAdPOuruYnTSFOXLv0I" type="module" crossorigin defer></script>
+    <script src="https://unpkg.com/tachyonjs@1.1.0/tachyon.min.js" integrity="sha384-CtVYSBNkcN7pv9PyX+bA3g2gO9x+hUB9SLdJPJXHf1c3cdE3ZAx6Rn7npdQJQ1C7" type="module" crossorigin defer></script>
     <?php
 }
 add_action('wp_head', 'wpb_hook_javascript');
